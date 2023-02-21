2e13cc6be479b19d8d52ae29eda63cc02fb552f3
barrosbina <barrosbina96@gmail.com>
2023-02-21 17:12:19 -0300

    Add pasta JDK

---
+++ JDK/PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+public class PrimeiroPrograma{
+	public static void main(String args[]) {
+		System.out.print("Hello World: ");
+	}
+}
+
+#Obs: comando javac nome programa compila arquivo (cria arquivo .class) .java. Para executar: java nome programa
